b6d7e1ec97d2d54a50ac7e78fd6dd02889dcdcea
Kojo Bailey <kojomolojo@gmail.com>
2026-02-17 22:39:46 +0000

    Create Version struct (needs to be put into separate module)

--- player-color-param/src/lib.rs
+++ player-color-param/src/lib.rs
@@ -1,3 +1,30 @@
+use std::fmt;
+
+// [TODO] Make separate module for all structs to use.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Version {
+    pub major: u32,
+    pub minor: u32,
+    pub patch: u32,
+}
+
+impl Default for Version {
+    fn default() -> Version {
+        Version { major: 1, minor: 0, patch: 0 }
+    }
+}
+
+impl fmt::Display for Version {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}.{}.{}", self.major, self.minor, self.patch)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct PlayerColorParam {
+    pub version: Version,
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub struct RGB {
     pub red: u8,
@@ -6,14 +33,14 @@ pub struct RGB {
 }
 
 #[derive(Debug, Clone)]
-pub struct PlayerColorParam {
+pub struct Entry {
     pub character_id: String,
     pub costume_index: u8,
     pub alt_index: u8,
     pub color: RGB,
 }
 
-impl PlayerColorParam {
+impl Entry {
     pub fn key(&self) -> String {
         format!("{}_{}_{}", self.character_id, self.costume_index, self.alt_index)
     }
