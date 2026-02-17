5647134210190e031bd4b546725de875e6f06b0e
Kojo Bailey <kojomolojo@gmail.com>
2026-02-17 23:53:55 +0000

    Move Version into its own module

--- player-color-param/src/lib.rs
+++ player-color-param/src/lib.rs
@@ -1,25 +1,5 @@
-use std::fmt;
+use version::Version;
 use std::collections::HashMap;
-
-// [TODO] Make separate module for all structs to use.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Version {
-    pub major: u32,
-    pub minor: u32,
-    pub patch: u32,
-}
-
-impl Default for Version {
-    fn default() -> Version {
-        Version { major: 1, minor: 0, patch: 0 }
-    }
-}
-
-impl fmt::Display for Version {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}.{}.{}", self.major, self.minor, self.patch)
-    }
-}
 
 #[derive(Debug, Clone)]
 pub struct PlayerColorParam {
