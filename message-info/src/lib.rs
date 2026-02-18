14e9ac704d69e838cc2999751f6cb163aba79c58
Kojo Bailey <kojomolojo@gmail.com>
2026-02-18 00:10:21 +0000

    Draft MessageInfo

---
+++ message-info/src/lib.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct MessageInfo {
+    pub language: Language,
+    pub entries: HashMap<u64, Entry>,
+}
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+pub struct Entry {
+    pub hash_id: u64,
+    pub string_id: Option<String>,
+    pub message: Option<String>,
+    pub reference_id: Option<String>,
+    pub adx2_file: Option<String>,
+    pub adx2_cue_index: Option<u64>,
+}
+
+impl Entry {
+    pub fn key(&self) -> u64 { self.hash_id }
+}
+
+#[derive(Debug, Clone)]
+pub enum Language {
+    English,
+    Spanish,
+    German,
+    Italian,
+    French,
+    Japanese,
+    Korean,
+    ChineseSimplified,
+    ChineseTraditionnal,
+    Other(String), // ISO 639 Set 3
+}
